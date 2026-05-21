dab8d107364272d031fe68d575e08fce034e5ced
Satyam Tripathi <185178986+satyam1978@users.noreply.github.com>
2026-05-21 20:48:46 +0530

    Db uri Change

--- DB_connection/src/db/db.js
+++ DB_connection/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
 
-  await mongoose.connect("bd_connection_string_uri/halley")
+  await mongoose.connect("db_connection_string_uri/halley")
 
   console.log("DB Connected");
 }
